934a32f27d6933d7337129e17fb0ffce93eed466
FELIPE AUGUSTO BORGES <felipeborges@univille.br>
2018-06-06 20:18:27 -0300

    projeto finalizado

--- src/main/java/br/univille/projdacs2018spring/controller/PacienteController.java
+++ src/main/java/br/univille/projdacs2018spring/controller/PacienteController.java
@@ -1,29 +1,58 @@
 package br.univille.projdacs2018spring.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.univille.projdacs2018spring.model.Paciente;
+import br.univille.projdacs2018spring.repository.PacienteRepository;
 
 @Controller
 @RequestMapping("/paciente")
 public class PacienteController {
 	
+	@Autowired
+    private PacienteRepository PacienteRepository;
+	
 	@GetMapping("")
 	public ModelAndView index() {
-		List<Paciente> listaPaciente = new ArrayList<Paciente>();
-        
-        Paciente p1 = new Paciente();
-        p1.setNome("Zezinho");
-        p1.setSexo("Masculino");
-        
-        listaPaciente.add(p1);
+		List<Paciente> listaPaciente = this.PacienteRepository.findAll();
         
         return new ModelAndView("paciente/index","listapac",listaPaciente);
     }
+	
+	 @GetMapping("/novo")
+	 public String createForm(@ModelAttribute Paciente Paciente) {
+	     return "paciente/form";
+	 }
+	 
+	 @PostMapping(params="form")
+	 public ModelAndView save(@Valid Paciente paciente, BindingResult result, RedirectAttributes redirect) {
+	        
+	    paciente = this.PacienteRepository.save(paciente);
+	        
+	    return new ModelAndView("redirect:/paciente");
+	 }
+	 
+	 @GetMapping(value="/alterar/{id}")
+	 public ModelAndView alterarForm(@PathVariable("id") Paciente paciente) {
+	     return new ModelAndView("paciente/form","paciente",paciente);
+	 }
+	 
+	 @GetMapping(value="remover/{id}")
+	 public ModelAndView remover(@PathVariable ("id") Long id, RedirectAttributes redirect) {
+	     this.PacienteRepository.deleteById(id);
+	     return new ModelAndView("redirect:/paciente");
+	 }
 }
